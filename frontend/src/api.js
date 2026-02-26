dc3f76ef60acc77fa253f610f630f32f1fc1b8e3
SVPraveen1 <shyamkaruparthi@gmail.com>
2026-02-26 17:25:09 +0530

    auth fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+  baseURL: "/api" ||import.meta.env.VITE_API_BASE_URL,
 });
 
 // Add a request interceptor to attach the JWT token
